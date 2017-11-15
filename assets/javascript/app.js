f002fdb3ab2f8f64b069327624ee84c2e8328505
Grant Dixon <gdixon1102@gmail.com>
2017-11-14 21:03:02 -0700

    yes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -72,59 +72,47 @@
   }
 
   
-  // ========================================================
-
-  // Generic function for displaying cartoon data 
+  
   function renderButtons(){ 
 
-    // Deletes the cartoons prior to adding new cartoons (this is necessary otherwise you will have repeat buttons)
+    
     $('#buttonsView').empty();
 
-    // Loops through the array of cartoons
+   
     for (var i = 0; i < animals.length; i++){
 
-      // Then dynamicaly generates buttons for each cartoon in the array
-
-      // Note the jQUery syntax here... 
-        var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-        a.addClass('animal'); // Added a class 
-        a.addClass("btn btn-success"); // Added a class 
+      
+      
+        var a = $('<button>') 
+        a.addClass('animal'); 
+        a.addClass("btn btn-success");  
         a.addClass("btn btn-primary btn-lg");
-        a.attr('data-name', animals[i]); // Added a data-attribute
-        a.text(animals[i]); // Provided the initial button text
-        $('#buttonsView').append(a); // Added the button to the HTML
+        a.attr('data-name', animals[i]); 
+        a.text(animals[i]); 
+        $('#buttonsView').append(a); 
     }
   }
 
-  // ========================================================
-
-  // This function handles events where one button is clicked
+  
   $('#addanimal').on('click', function(){
 
-    //clear container
-    //$('#cartoonsView').html("");     
-
-    // This line of code will grab the input from the textbox
+    
     var animal = $('#animal-input').val().trim();
 
-    // The cartoon from the textbox is then added to our array
+    
     animals.push(animal);
     
-    // Our array then runs which handles the processing of our cartoon array
+    
     renderButtons();
 
-    // We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+    
     return false;
   })
 
-  // ========================================================
-
-  // Generic function for displaying the cartoonInfo
+  
   $(document).on('click', '.animal', displayanimalInfo);
 
 
-  // ========================================================
-
-  // This calls the renderButtons() function
+  
   renderButtons();
-  //displaycartoonInfo();
+  
